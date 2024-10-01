f5b434c649181cc2058fa40c37724bc4468a8c17
Juan Tornay <120737352+juan-Tornay@users.noreply.github.com>
2024-10-01 16:42:26 +0200

    actualizacion

--- 04nodejs/SevenBoom.js
+++ 04nodejs/SevenBoom.js
@@ -1,5 +1,16 @@
-for(let i=0;i<=100;i++){
+function sevenboom(){
+    for(let i=1;i<=100;i++){
 
-  console.log(i)
 
+        if(i.toString().includes('7')){
+            console.log('boom')
+
+        }else{
+            console.log(i);
+        }
+    }
 }
+
+
+sevenboom();
+
