f3712eaefe8ca1d3c0770c1042820e16da2a3ea1
Juan Tornay <120737352+juan-Tornay@users.noreply.github.com>
2024-10-01 17:23:55 +0200

    actualizacion

---
+++ 04nodejs/OdishVsEvenish.js
@@ -0,0 +1,14 @@
+function parImpar(){
+    
+    for(let i=1;i<=100;i++){
+       
+       if(i% 2!== 0){
+            console.log('Oddish')
+       }
+       
+        if(i%2 === 0){
+            console.log('Evenish')
+        }
+     }
+}
+parImpar();
