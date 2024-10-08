5c6ba563c5ae19220e154c3fa66b6c4651d59f29
Juan Tornay <120737352+juan-Tornay@users.noreply.github.com>
2024-10-08 19:49:46 +0200

    actualizacion

---
+++ 06nodejs/Fichero.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const Archivillo= 'C:\Users\jua24\Documents\GitHub\DWES\06nodejs\Ficherio.txt';
+
+
+
+const imprimir = path.dirname(Archivillo);
+
+console.log(imprimir);
