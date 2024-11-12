bfd8da97d88126addc03959dd557374aeeae6c1b
Juan Tornay <120737352+juan-Tornay@users.noreply.github.com>
2024-11-12 20:26:46 +0100

    actualizacion

--- 016nodejs/server.js
+++ 016nodejs/server.js
@@ -1,27 +1,34 @@
 const http = require('http');
 const { Logger } = require('winston');
 
-// Define el puerto en el que el servidor escuchará
 const PORT = 3000;
 
-// Crea el servidor y define la función que manejará las solicitudes
 
 
-const palabra =  'que dise canijo'
+
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200; // Código de estado de éxito
-  res.setHeader('Content-Type', 'text/plain'); // Tipo de contenido
-  res.end('Esto es el servidor del ejercicio1 de la practica 16'); 
+  res.statusCode = 200; 
+  res.setHeader('Content-Type', 'text/plain'); 
+ 
 
- //res.end('ME gustan los botellines');
-
+  if (req.url === '/') {
+ 
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('server Error');
+  } else {
+   
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Esto es el servidor del ejercicio 1 de la práctica 16');
+  }
 });
 
-throw new Error('Server Error');
 
 
-// Hace que el servidor escuche en el puerto especificado
+
+
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
